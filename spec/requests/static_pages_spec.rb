9dd654c269128600487062685078e847a83b2d6d
prog <prog@app01.ru>
2014-03-22 12:13:41 +0400

    finish chapter 3

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,8 +3,7 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-let(:fixed_text){"Ruby on Rails Tutorial Sample App |"}
-
+let(:fixed_text){"Ruby on Rails Tutorial Sample App | "}
 
   describe "Home page" do
 
@@ -13,10 +12,15 @@ let(:fixed_text){"Ruby on Rails Tutorial Sample App |"}
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
-  	  visit '/static_pages/home'
-  	  expect(page).to have_title(fixed_text + " Home")
-  	end
+    it "should have the base title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
 
   end
 
